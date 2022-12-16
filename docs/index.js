6b8833121d1dd0ec8f8f04da1f8216bdc0abbc41
UbaidRussell <UbaidRussell@users.noreply.github.com>
2022-12-15 23:45:24 -0500

    Linked images and changed our heading color

--- docs/index.js
+++ docs/index.js
@@ -3,42 +3,42 @@ randomNumber2 = Math.floor(Math.random() * (7 - 1) + 1);
 
 //Left images
 if (randomNumber1 === 1){
-    document.getElementsByTagName("img")[0].setAttribute("src", "docs/images/dice1.png");
+    document.getElementsByTagName("img")[0].setAttribute("src", "docs/images/black dice one.png");
   }
   else if (randomNumber1 === 2){
-    document.getElementsByTagName("img")[0].setAttribute("src", "docs/images/dice2.png");
+    document.getElementsByTagName("img")[0].setAttribute("src", "docs/images/black dice 2.png");
   }
   else if(randomNumber1 === 3){
-    document.getElementsByTagName("img")[0].setAttribute("src", "docs/images/dice3.png");
+    document.getElementsByTagName("img")[0].setAttribute("src", "docs/images/black dice 3.png");
   }
   else if (randomNumber1 === 4) {
-    document.getElementsByTagName("img")[0].setAttribute("src", "docs/images/dice4.png");
+    document.getElementsByTagName("img")[0].setAttribute("src", "docs/images/black dice 4.png");
   }
   else if (randomNumber1 === 5){
-    document.getElementsByTagName("img")[0].setAttribute("src", "docs/images/dice5.png");
+    document.getElementsByTagName("img")[0].setAttribute("src", "docs/images/black dice 5.png");
   }
   else {
-    document.getElementsByTagName("img")[0].setAttribute("src", "docs/images/dice6.png");
+    document.getElementsByTagName("img")[0].setAttribute("src", "docs/images/black dice 6.png");
 }
 
 //Right images
 if (randomNumber2 === 1){
-  document.getElementsByTagName("img")[1].setAttribute("src", "docs/images/dice1.png");
+  document.getElementsByTagName("img")[1].setAttribute("src", "docs/images/black dice one.png");
 }
 else if (randomNumber2 === 2){
-  document.getElementsByTagName("img")[1].setAttribute("src", "docs/images/dice2.png");
+  document.getElementsByTagName("img")[1].setAttribute("src", "docs/images/black dice 2.png");
 }
 else if(randomNumber2 === 3){
-  document.getElementsByTagName("img")[1].setAttribute("src", "docs/images/dice3.png");
+  document.getElementsByTagName("img")[1].setAttribute("src", "docs/images/black dice 3.png");
 }
 else if (randomNumber2 === 4) {
-  document.getElementsByTagName("img")[1].setAttribute("src", "docs/images/dice4.png");
+  document.getElementsByTagName("img")[1].setAttribute("src", "docs/images/black dice 4.png");
 }
 else if (randomNumber2 === 5){
-  document.getElementsByTagName("img")[1].setAttribute("src", "docs/images/dice5.png");
+  document.getElementsByTagName("img")[1].setAttribute("src", "docs/images/black dice 5.png");
 }
 else {
-  document.getElementsByTagName("img")[1].setAttribute("src", "docs/images/dice6.png");
+  document.getElementsByTagName("img")[1].setAttribute("src", "docs/images/black dice 6.png");
 }
 
 //Heading change
